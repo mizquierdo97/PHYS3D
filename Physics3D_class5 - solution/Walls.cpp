488178ad4da84faaf8ee0e37ca024e09d0206b28
aliciadr <alicia_dr@hotmail.es>
2016-12-14 18:34:08 +0100

    more walls p2

--- Physics3D_class5 - solution/Walls.cpp
+++ Physics3D_class5 - solution/Walls.cpp
@@ -43,7 +43,40 @@ void ModuleSceneIntro::Walls() {
 	AddWall(84, 2, -106, 5, -2);
 	AddWall(93, 2, -101, 10, 2);
 
-	AddWall(99, 2, -83, 10, 1);
-	AddWall(115, 2, -83, 10, 1);
+	//--------------------------//
 
+	AddWall(99, 2, -74, 20, 1);
+	AddWall(78, 2, -26, 35, -2);
+	AddWall(92, 2, -18, 25, -2);
+
+			//DCH
+	AddWall(60, 2, 15, 30, 1);
+	AddWall(77, 2, 23, 22, 1);
+	AddWall(92, 2, 93, 35, 2);  // PARED RARA BOLA RODANTE
+	AddWall(104, 2, 80, 45, 2);
+	AddWall(113, 2, 132, 12, 1);
+	AddWall(140, 2, 134, 10, 1);
+
+
+			//IZQ
+	AddWall(115, 2, -74, 20, 1);
+	AddWall(124, 2, -42, 15, 2);
+	AddWall(113, 2, -30, 10, 2);
+	AddWall(119, 2, -2, 20, 1);
+	AddWall(133, 2, -10, 20, 1);
+
+	AddWall(139, 2, 18, 10, 2);
+	AddWall(131, 2, 34, 24, 2);
+	AddWall(158, 2, 34, 24, -2);
+	AddWall(151, 2, 18, 10, -2);
+	AddWall(172, 2, 0, 20, 1);
+	AddWall(157, 2, -10, 20, 1);
+
+	AddWall(165, 2, -35, 30, -2);
+	AddWall(178, 2, -27, 10, -2);
+	AddWall(184, 2, 5, 40, 1);
+	AddWall(157, 2, 80, 45, -2);
+	AddWall(190, 2, -45, 20, 2);
+	AddWall(200, 2, 5, 40, 1);
+	AddWall(173, 2, 80, 55, -2);
 }
